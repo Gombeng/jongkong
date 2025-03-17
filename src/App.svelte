9b83454d8b45b8d0cef6ae25001ce5837c4d94e7
Gombeng <msrgombeng@gmail.com>
2025-03-17 15:17:22 +0700

    redirect to dashboard

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,13 @@
 <script>
   import Router from "svelte-spa-router";
   import { routes } from "./utils";
+  import { onMount } from "svelte";
+
+  onMount(() => {
+    if (window.location.pathname === "/" && !window.location.hash) {
+      window.location.href = "/#/dashboard";
+    }
+  });
 </script>
 
 <main data-theme="dark">
