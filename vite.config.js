4c407165436f0335d63be6b68d541d9b7218041f
Gombeng <msrgombeng@gmail.com>
2025-03-17 15:09:03 +0700

    edit file

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte(), tailwindcss()],
-  optimizeDeps: {
-    include: ["zod"],
+  build: {
+    rollupOptions: {
+      external: ["zod"],
+    },
   },
 });
